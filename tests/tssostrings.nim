ed51f3fbe1384139f5f61cc0937d1a0acea50f08
Antonis Geralis <b3lievem@yandex.com>
2022-04-27 22:32:05 +0300

    add tests

---
+++ tests/tssostrings.nim
@@ -0,0 +1,21 @@
+import ssostrings
+
+proc main =
+  block:
+    let data = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    var str: String
+    for c in data:
+      str.add c
+    let expected = toStr(data)
+    assert(str.len == data.len)
+    assert(str == expected)
+  block:
+    let data = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for i in 0..data.len:
+      let strLen = data.len - i
+      let expected = data[0..<strLen]
+      let str = toStr(expected)
+      assert(str.len == strLen)
+      assert($str.toCstr == expected)
+
+main()
