2698811b1ac9aee0920f26154691771f4758a307
Antonis Geralis <b3lievem@yandex.com>
2022-11-18 17:48:51 +0200

    use explicit init

--- tests/tssostrings.nim
+++ tests/tssostrings.nim
@@ -7,27 +7,27 @@ proc parseHex[T: SomeInteger](s: String, number: var T, start = 0,
 proc main =
   block:
     let data = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    var str: String
+    var str: String = toStr""
     for c in data:
       str.add c
     let expected = toStr(data)
-    assert(str.len == data.len)
-    assert(str == expected)
+    assert str.len == data.len
+    assert str == expected
   block:
     let data = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    var a: seq[String]
+    var a: seq[String] = @[]
     for i in 0..data.len:
       let strLen = data.len - i
       let expected = data[0..<strLen]
       var str = toStr(expected)
       a.add(str)
-      assert(str.toCStr == expected.cstring)
-      assert(str.len == strLen)
+      assert str.toCStr == expected.cstring
+      assert str.len == strLen
     for i, str in enumerate(items(a)):
       let strLen = data.len - i
       let expected = data[0..<strLen]
-      assert(str.toCStr == expected.cstring)
-      assert(str.len == strLen)
+      assert str.toCStr == expected.cstring
+      assert str.len == strLen
   block:
     var str = toStr(cstring"7B")
     var num = 0
